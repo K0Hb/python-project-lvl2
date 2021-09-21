c63c4585366e6ed5c4dc372686fe61dea8c441ef
Vjachesalv Konovalov <goplit2010.konovalov@yandex.ru>
2021-09-21 10:33:36 +0300

    division of logic parser and open_file

--- gendiff/parser_args_terminal.py
+++ gendiff/parser_args_terminal.py
@@ -5,6 +5,7 @@ def cli_parser():
     parser = argparse.ArgumentParser(description='Generate diff')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
-    parser.add_argument('-f', '--format', default='stylish', choices=['stylish', 'plain', 'json'],
+    parser.add_argument('-f', '--format', default='stylish',
+                        choices=['stylish', 'plain', 'json'],
                         help='set format of output (default: stylish)')
     return parser.parse_args()
--- gendiff/parser_files.py
+++ gendiff/parser_files.py
@@ -3,12 +3,16 @@ import json
 import yaml
 
 
-def parse_file(file_path):
+def parser(file_format):
     format = {
         '.json': json.loads,
         '.yaml': yaml.safe_load,
         '.yml': yaml.safe_load
     }
+    return format[file_format.lower()]
+
+
+def parse_file(file_path):
     file_format = os.path.splitext(file_path)[1]
     with open(os.path.abspath(file_path)) as f:
-        return format[file_format](f.read())
+        return parser(file_format)(f.read())
