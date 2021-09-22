2e7d25f413c815b2ed4cfdb56c748bf212dc339c
Vjachesalv Konovalov <goplit2010.konovalov@yandex.ru>
2021-09-22 10:54:47 +0300

    simplifying the formation logic

--- gendiff/format/plain.py
+++ gendiff/format/plain.py
@@ -9,6 +9,12 @@ from gendiff.format.stylish import (
 
 def to_plain(difference, key_path=None):
     diff = []
+    flags = {
+        ADDED: add_added_node,
+        REMOVED: add_removed_node,
+        CHANGED: add_changed_node,
+        NESTED: add_nested_node,
+    }
 
     if not key_path:
         key_path = []
@@ -17,21 +23,21 @@ def to_plain(difference, key_path=None):
         flag, rest = value[0], value[1:]
         if flag == UNCHANGED:
             continue
-        string_diff(key, flag, rest, diff, key_path)
+        flags[flag](key, diff, key_path, rest)
     return '\n'.join(diff)
 
 
-def add_added_node(key, value, diff, key_path):
+def add_added_node(key, diff, key_path, rest):
     key_path.append(key)
     diff.append(
         'Property \'{0}\' was added with value: {1}'.format(
-            '.'.join(key_path), render_value(value[0])
+            '.'.join(key_path), render_value(rest[0])
         )
     )
     key_path.pop()
 
 
-def add_removed_node(key, key_path, diff):
+def add_removed_node(key, diff, key_path, rest=None):
     key_path.append(key)
     diff.append(
         'Property \'{}\' was removed'.format(
@@ -41,35 +47,21 @@ def add_removed_node(key, key_path, diff):
     key_path.pop()
 
 
-def add_changed_node(key, value, diff, key_path):
+def add_changed_node(key, diff, key_path, rest):
     key_path.append(key)
     diff.append(
         'Property \'{0}\' was updated. From {1} to {2}'.format(
-            '.'.join(key_path), render_value(value[0]),
-            render_value(value[1])
+            '.'.join(key_path), render_value(rest[0]),
+            render_value(rest[1])
         )
     )
     key_path.pop()
 
 
-def add_nested_node(key, value, diff, key_path):
+def add_nested_node(key, diff, key_path, rest):
     key_path.append(key)
-    diff.append(to_plain(value[0], key_path))
+    diff.append(to_plain(rest[0], key_path))
     key_path.pop()
-
-
-def string_diff(key, flag, rest, diff, key_path):
-    flags = {
-        ADDED: add_added_node,
-        REMOVED: add_removed_node,
-        CHANGED: add_changed_node,
-        NESTED: add_nested_node,
-    }
-
-    if flag == REMOVED:
-        flags[flag](key, key_path, diff)
-    else:
-        flags[flag](key, rest, diff, key_path)
 
 
 def render_value(value):
