b739e966b10a4f29fec03085bccd904517754af7
Vjachesalv Konovalov <goplit2010.konovalov@yandex.ru>
2021-09-22 14:42:57 +0300

    add text

--- gendiff/parser_args_terminal.py
+++ gendiff/parser_args_terminal.py
@@ -7,5 +7,5 @@ def cli_parser():
     parser.add_argument('second_file')
     parser.add_argument('-f', '--format', default='stylish',
                         choices=['stylish', 'plain', 'json'],
-                        help='set format of output (default: stylish)')
+                        help='set format of output by selecting from[stylish, plain, json]  (default: stylish)')
     return parser.parse_args()
